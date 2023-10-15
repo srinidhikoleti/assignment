23383dd724be6ad4cac23cebaa1eb5c87a451794
srinidhikoleti <115999926+srinidhikoleti@users.noreply.github.com>
2023-10-15 15:05:03 +0530

    Add files via upload

---
+++ js/reverseletters.js
@@ -0,0 +1,7 @@
+let string = prompt("enter string:");
+let words = string.split(' ')
+const reverse = words.map(word=>{
+    return word.split('').reverse().join('')
+})
+const revSen= reverse.join(' ')
+console.log(revSen)
